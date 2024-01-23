77a0a2a31d66f4ec9bcd0f9f91754c0cb3427352
Vitaliy Boyanivskyy <vitaliy.boyanivsky@scalors.com>
2024-01-23 19:52:38 +0200

    htmx go echo

---
+++ cmd/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
+type Contact struct {
+	Name string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact {
+		Name: name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d Data) hasEmail(email string) bool {
+	for _, c := range d.Contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: Contacts{
+			newContact("Vitaliy", "vitalik.boyan@gmail.com"),
+			newContact("John", "john@email.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	page := newPage()
+	e.Renderer = newTemplate()
+
+	e.GET("/", func (c echo.Context) error {
+		return c.Render(200, "index", page)
+	})
+
+	e.POST("/contacts", func (c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Errors["email"] = "Email already exists"
+			formData.Values["name"] = name
+			formData.Values["email"] =email 
+
+			// 422 error is correct with htmx
+			return c.Render(422, "form", formData)
+		}
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		c.Render(200, "form", newFormData())
+
+		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
